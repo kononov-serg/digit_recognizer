4a21fe8aef51f2a261216e8bc25268322be56157
Sergei Kononov <sergii_kononov1@epam.com>
2020-04-25 15:49:41 +0300

    Add new comment

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+import pandas as pd
+#test comment:
+train_df = pd.read_csv("train.csv")
+test_df = pd.read_csv("test.csv")
 
 sess = tf.InteractiveSession()
 
@@ -20,11 +21,15 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y,
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 for i in range(1000):
-  batch = mnist.train.next_batch(100)
-  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+  x_batch = train_df.iloc[range(i, i + 10), range(1, train_df.shape[1])]
+  y_batch = train_df.iloc[range(i, i + 10), 0]
+  train_step.run(feed_dict={x: x_batch, y_: y_batch})
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
+# ImageId, Label
